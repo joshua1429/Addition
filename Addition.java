1464c5c241226b3b43a368e45cf2029f1750f28a
joshua1429 <103485143+joshua1429@users.noreply.github.com>
2022-04-28 06:40:28 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,7 @@
+class Addition{
+      public static void main(String[]args){
+          int first_number=8,second_number=6,result;
+          result=first_number + second_number;
+          System.out.println("The Addition of two number is:"+result);
+      }
+}
